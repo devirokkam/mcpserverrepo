af468db5242d86aae38e4a1db807058344c4c68f
devirokkam <devirokkam49@gmail.com>
2025-11-10 22:07:07 +0530

    mcpserverdeployment

---
+++ src/mcpserver/__main__.py
@@ -0,0 +1,5 @@
+from mcpserver.deployment import mcp
+def main():
+    mcp.run()
+if __name__ == "__main__":
+    main()
---
+++ src/mcpserver/deployment.py
@@ -0,0 +1,11 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+#uvx --from git+https://github.com/gvbigdata/MCP-Server-Example.git mcp-server
